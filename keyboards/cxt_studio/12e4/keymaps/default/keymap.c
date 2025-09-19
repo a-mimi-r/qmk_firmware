677492fff60e26627e782c8d47cd5d12fcb78602
Mari <marina.rubinato@icloud.com>
2025-09-19 03:34:03 -0300

    Add files via upload

--- keyboards/cxt_studio/12e4/keymaps/default/keymap.c
+++ keyboards/cxt_studio/12e4/keymaps/default/keymap.c
@@ -6,3 +6,31 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [3] = { ENCODER_CCW_CW(UG_PREV, UG_NEXT),  ENCODER_CCW_CW(KC_RIGHT, KC_LEFT), ENCODER_CCW_CW(UG_VALD, UG_VALU),  ENCODER_CCW_CW(UG_SPDD, UG_SPDU)  },
 };
 #endif
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    uint8_t index_to_light = NO_LED;
+
+    switch (get_highest_layer(layer_state | default_layer_state)) {
+        case 1:
+            index_to_light = 3;
+            break;
+        case 2:
+            index_to_light = 2;
+            break;
+        case 3:
+            index_to_light = 1;
+            break;
+        case 4:
+            index_to_light = 0;
+            break;
+        default:
+            break;
+    }
+
+    // Light up only the selected LED if it's within the current render range
+    if (index_to_light != NO_LED && index_to_light >= led_min && index_to_light < led_max) {
+        rgb_matrix_set_color(index_to_light, RGB_GREEN); // You can choose any color
+    }
+
+    return false;  // Let other lighting effects (if any) still apply
+}
--- keyboards/cxt_studio/12e4/keymaps/via/rules.mk
+++ keyboards/cxt_studio/12e4/keymaps/via/rules.mk
@@ -1,5 +1,10 @@
 RGB_MATRIX_DRIVER = ws2812
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_KEYPRESSES = yes                 # Enables reactive typing effects
+RGB_MATRIX_FRAMEBUFFER_EFFECTS = yes        # Enables animated effects
 ENCODER_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
 
 VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIALRGB_ENABLE = yes
